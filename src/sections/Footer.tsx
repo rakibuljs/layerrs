6163454790838f401c1a56c91c0104a6ec38a2fd
rakibul <rakibuldevs@gmail.com>
2025-02-26 08:45:05 +0600

    add footer section add to the website

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,35 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+const footerLinks = [
+  { href: "#", label: "Contact" },
+  { href: "#", label: "Privacy Policy" },
+  { href: "#", label: "Terms & Conditions" },
+];
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <section className="py-16">
+      <div className="container">
+        <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+          <div>
+            <Image src={logoImage} alt="layers logo" />
+          </div>
+          <div>
+            <nav className="flex gap-6 ">
+              {footerLinks.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.label}
+                  className="text-white/50 text-sm hover:text-lime-400 transition-colors duration-500"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
